8154d91c200ec78a79e8d77801bd2e5ae094924b
Santhosh Kumar K <ksk.santhosh21@gmail.com>
2021-11-18 10:19:38 +0530

    login DONE

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+    def after_sign_in_path_for(resource)
+        # check for the class of the object to determine what type it is
+        case resource.class
+        when @farmer
+          farmer_path  
+        when @officer
+          officer_path
+        end
+    end
+
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+  def farmer
+  end
+  def officer
+  end
 end
